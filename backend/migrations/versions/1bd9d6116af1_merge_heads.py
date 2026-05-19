6806362cc8078833cb4bbc095ece590c073670b7
zzx34 <18457093896@163.com>
2026-05-19 14:43:23 +0800

    合并

---
+++ backend/migrations/versions/1bd9d6116af1_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 1bd9d6116af1
+Revises: 9f1c2c2e7b1a, g4c2d8e9f5a1
+Create Date: 2026-05-19 14:18:59.888099
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1bd9d6116af1'
+down_revision = ('9f1c2c2e7b1a', 'g4c2d8e9f5a1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
